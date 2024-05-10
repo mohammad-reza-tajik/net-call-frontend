4da698d339b37252e582ae34be40e77fbb397ea2
Morta <mr.tajik1999@gmail.com>
2024-05-10 15:31:59 +0330

    refactor(Icons): change Hamburger icon to material design light version

--- components/shared/Icons.tsx
+++ components/shared/Icons.tsx
@@ -193,10 +193,9 @@ export function Handshake(props: SVGProps<SVGSVGElement>) {
     )
 }
 
-
 export function Hamburger(props: SVGProps<SVGSVGElement>) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M4 17.27v-1h16v1zm0-4.77v-1h16v1zm0-4.77v-1h16v1z"></path></svg>
     )
 }
 
