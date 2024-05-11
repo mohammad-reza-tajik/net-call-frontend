ea18f8ab5949bdce9e2b864157d28f93f22156f1
Morta <mr.tajik1999@gmail.com>
2024-05-11 08:00:44 +0330

    feat : add PeerId component
    this shows the user Id with the ability to copy the id by clicking

---
+++ components/shared/PeerId.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import {useAppSelector} from "@/store";
+import {Button} from "@/components/ui/button";
+
+function PeerId() {
+
+    const peerId = useAppSelector(state => state.peer.peerId);
+
+    return (
+        <div className={"flex items-center justify-center gap-2 text-sm"}>
+
+            آیدی شما :
+            
+            {
+                peerId &&
+                <Button variant={"outline"} onClick={() => navigator.clipboard.writeText(peerId)}>
+                    {peerId}
+                </Button>
+            }
+        </div>
+    )
+}
+
+export default PeerId;
