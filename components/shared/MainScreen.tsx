88a7fe4b4677458be41818cc950d04e51f112b3e
Morta <mr.tajik1999@gmail.com>
2024-05-11 13:26:01 +0330

    refactor : initialize peerConnection & videoEl on startup

--- components/shared/MainScreen.tsx
+++ components/shared/MainScreen.tsx
@@ -1,29 +1,38 @@
 "use client"
 
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 import createId from "@/utils/createId";
-import {peerActions, useAppDispatch} from "@/store";
+import {peerActions, useAppDispatch, useAppSelector} from "@/store";
 import createConnection from "@/utils/createConnection";
 import io from "socket.io-client";
+import {cn} from "@/lib/utils";
 
 function MainScreen() {
 
-
+    const peer = useAppSelector(state => state.peer);
+    const { status } = peer;
     const dispatch = useAppDispatch();
+    const videoRef = useRef<HTMLVideoElement | null>(null);
 
     useEffect(() => {
         (async () => {
             const peerId = createId();
+            const peerConnection = await createConnection({dispatch, peer});
+
             const socketConnection = io("http://localhost:3001").connect();
-            dispatch(peerActions.setPeer({socket: socketConnection, peerId}));
+
+            dispatch(peerActions.setPeerConnection(peerConnection));
+            dispatch(peerActions.setPeerId(peerId));
+            dispatch(peerActions.setSocket(socketConnection));
+            dispatch(peerActions.setVideoRef(videoRef));
+
         })();
 
     }, []);
 
-
     return (
         <section className={"flex flex-1 items-center justify-around"}>
-            {/*<video ref={videoRef} controls playsInline autoPlay className={cn("size-full",{"hidden": !status})} />*/}
+            <video ref={videoRef} controls autoPlay className={cn("size-full", {"hidden": !status})}/>
         </section>
     )
 }
