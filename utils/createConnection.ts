db0dcd6a37ae024c0d3bdc44e599477e4b10c03f
Morta <mr.tajik1999@gmail.com>
2024-05-12 09:17:00 +0330

    feat(createConnection): listen for track event after creating connection

--- utils/createConnection.ts
+++ utils/createConnection.ts
@@ -5,18 +5,25 @@ import {ThunkDispatch} from "redux-thunk";
 
 async function createConnection({dispatch, peer}: { dispatch: ThunkDispatch, peer: Peer }) {
 
-    const peerConfig = { iceServers: [{ urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] }] }
+    const peerConfig = {iceServers: [{urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"]}]}
     const peerConnection = new RTCPeerConnection(peerConfig);
 
     peerConnection.addEventListener("icecandidate", (event) => {
         if (event.candidate) {
-            console.log(event.candidate);
             dispatch(peerActions.addIceCandidate(event.candidate));
         }
     });
 
-    peerConnection.addEventListener("signalingstatechange", (event) => {
+    /*peerConnection.addEventListener("signalingstatechange", (event) => {
         console.log(peerConnection.signalingState);
+    })*/
+
+    peerConnection.addEventListener("track", (event) => {
+        console.log(event);
+        /*if (videoRef?.current) {
+            videoRef.current.srcObject = event.streams[0];
+
+        }*/
     })
 
     return peerConnection;
