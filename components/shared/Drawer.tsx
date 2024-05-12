731e972e053f8aca748c61e742e0e3d7bcd339cd
Morta <mr.tajik1999@gmail.com>
2024-05-12 09:33:37 +0330

    feat(Drawer): show requests in the drawer and answer them by clicking

--- components/shared/Drawer.tsx
+++ components/shared/Drawer.tsx
@@ -1,16 +1,21 @@
 "use client"
-import {drawerActions, useAppDispatch, useAppSelector} from "@/store";
+import {drawerActions, peerActions, useAppDispatch, useAppSelector} from "@/store";
 import {cn} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import {Close} from "@/components/shared/Icons";
+import createAnswer from "@/utils/createAnswer";
+import {Request} from "@/types";
 
 function Drawer() {
 
     const dispatch = useAppDispatch();
     const drawerIsOpen = useAppSelector(state => state.drawer.isOpen);
+    const peer = useAppSelector(state => state.peer);
+    const {requests} = peer
 
-    const menuItemsHandler = (url: string) => {
-        dispatch(drawerActions.closeDrawer());
+    async function answerHandler(request: Request) {
+        dispatch(peerActions.setStatus("receiveScreen"));
+        const answer = await createAnswer({dispatch, peer, request});
     }
 
     return (
@@ -21,18 +26,19 @@ function Drawer() {
             )}
             <div
                 className={cn("fixed top-0 left-0 h-full w-3/4 sm:w-1/4 flex flex-col gap-2 p-5 bg-background z-50 transition-transform ease-in-out duration-300", {"-translate-x-full": !drawerIsOpen})}>
-                <Button size={"icon"} variant={"outline"} onClick={() => dispatch(drawerActions.closeDrawer())} aria-label={"بستن منو"}>
+                <Button size={"icon"} variant={"outline"} onClick={() => dispatch(drawerActions.closeDrawer())}
+                        aria-label={"بستن منو"}>
                     <Close/>
                 </Button>
                 <h1 className={"my-5 bg-primary text-center p-3 rounded"}>
                     دستگاه های قابل دسترس
                 </h1>
-                {
-                    Array.from({length : 7}).map((_, index) => {
-                        return <button key={index}
-                                       className={"flex items-center text-xs sm:text-sm gap-3 rounded hover:bg-foreground/5 border-b p-2 sm:p-4 hover:fill-primary transition-colors"}>
-                            {Math.random()}
-                        </button>
+                {!requests || requests.length === 0 ? <p>هیچ دستگاهی وجود ندارد</p> :
+                    requests.map((request) => {
+                        return <Button variant={"ghost"} size={"sm"} key={request.peerId}
+                                       onClick={() => answerHandler(request)}>
+                            {request.peerId}
+                        </Button>
                     })
                 }
             </div>
