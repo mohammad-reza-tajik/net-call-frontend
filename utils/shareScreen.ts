b4d1e1b46a196e5b606efc55a5f799501c38d458
Morta <mr.tajik1999@gmail.com>
2024-05-13 06:38:14 +0330

    feat: add audio to share screen stream

--- utils/shareScreen.ts
+++ utils/shareScreen.ts
@@ -3,18 +3,29 @@ import {peerActions} from "@/store";
 import {type ThunkDispatch} from "redux-thunk";
 import {Peer} from "@/types";
 
-export async function shareScreen({dispatch, peer} : {dispatch : ThunkDispatch , peer : Peer}) {
+export async function shareScreen({dispatch, peer}: { dispatch: ThunkDispatch, peer: Peer }) {
     try {
-        const {  videoRef ,peerConnection} = peer;
-        const myScreen = await navigator.mediaDevices.getDisplayMedia({audio: true, video: true});
+        const {peerConnection} = peer;
+
+        const audioStream = await navigator.mediaDevices.getUserMedia({audio: true});
+        const screenStream = await navigator.mediaDevices.getDisplayMedia({audio: true, video: true});
         dispatch(peerActions.setStatus("screen:send"));
-        myScreen.getTracks().forEach(track => {
-            peerConnection?.addTrack(track, myScreen);
+
+        const audioTrack = audioStream.getAudioTracks().at(0);
+
+        if (audioTrack) {
+            screenStream.addTrack(audioTrack!)
+
+        }
+
+        screenStream.getTracks().forEach(track => {
+            peerConnection?.addTrack(track, screenStream);
         });
+
         const offer = await peerConnection?.createOffer();
         await peerConnection?.setLocalDescription(offer);
 
-        dispatch(peerActions.setStream(myScreen));
+        dispatch(peerActions.setStream(screenStream));
         dispatch(peerActions.setPeerConnection(peerConnection));
         dispatch(peerActions.setOffer(offer));
 
