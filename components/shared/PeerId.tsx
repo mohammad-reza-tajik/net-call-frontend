07616f4c63ebf49828af9087cd145cfd4fa9d992
Morta <mr.tajik1999@gmail.com>
2024-05-13 08:30:00 +0330

    feat(PeerId): add ability to copy the peer id and get a success toast

--- components/shared/PeerId.tsx
+++ components/shared/PeerId.tsx
@@ -2,23 +2,42 @@
 
 import {useAppSelector} from "@/store";
 import {Button} from "@/components/ui/button";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import {toast} from "react-toastify";
 
 function PeerId() {
 
     const peerId = useAppSelector(state => state.peer.peerId);
 
+    async function copyIdHandler () {
+        await navigator.clipboard.writeText(peerId!);
+        toast("آیدی کپی شد");
+    }
+
     return (
-        <div className={"flex items-center justify-center gap-2 text-sm"}>
+        <TooltipProvider>
 
-            آیدی شما :
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <div className={"flex items-center justify-center gap-2 text-sm"}>
+                        آیدی شما :
 
-            {
-                peerId &&
-                <Button variant={"outline"} onClick={() => navigator.clipboard.writeText(peerId)}>
-                    {peerId}
-                </Button>
-            }
-        </div>
+                        {
+                            peerId &&
+                            <Button variant={"outline"} onClick={copyIdHandler}>
+                                {peerId}
+                            </Button>
+                        }
+
+                    </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>برای کپی کردن آیدی کلیک کنید</p>
+                </TooltipContent>
+            </Tooltip>
+
+
+        </TooltipProvider>
     )
 }
 
