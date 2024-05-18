013d184b9327fb663ddc22341c5e66cd638e6294
Morta <mr.tajik1999@gmail.com>
2024-05-18 22:11:49 +0330

    feat: add DeviceSelector component

---
+++ components/shared/DeviceSelector.tsx
@@ -0,0 +1,41 @@
+"use client"
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {useState} from "react";
+
+interface Props {
+    devices : MediaDeviceInfo[]
+}
+
+function DeviceSelector({devices}: Props) {
+
+    if(!devices) {
+        return
+    }
+
+    const [device, setDevice] = useState("default");
+
+    const sortChangeHandler = (value: string) => {
+        setDevice(value);
+    }
+
+    return (
+        <Select onValueChange={sortChangeHandler} defaultValue={device}>
+            <SelectTrigger className={"w-36 md:w-44"}>
+                <SelectValue>
+                    {device}
+                </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+                {
+                    devices.map((item) => {
+                        return (
+                            <SelectItem value={item.deviceId} key={item.deviceId}>{item.label}</SelectItem>
+                        )
+                    })
+                }
+            </SelectContent>
+        </Select>
+    )
+}
+
+export default DeviceSelector;
