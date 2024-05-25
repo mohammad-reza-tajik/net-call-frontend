ae55be2c1849d96b02b501e43f2f9a7c84f29743
Morta <mr.tajik1999@gmail.com>
2024-05-25 19:40:41 +0330

    feat: add connect page & connect Screen

---
+++ app/connect/page.tsx
@@ -0,0 +1,12 @@
+import ConnectScreen from "@/components/screens/ConnectScreen";
+
+function ConnectPage() {
+
+    return (
+        <main className={"flex flex-1 flex-col py-5"}>
+            <ConnectScreen />
+        </main>
+    )
+}
+
+export default ConnectPage;
--- components/screens/ConnectScreen.tsx
+++ components/screens/ConnectScreen.tsx
@@ -1,47 +1,57 @@
 "use client"
 
-import useInitialize from "@/hooks/useInitialize";
-import useSocket from "@/hooks/useSocket";
-import {useAppSelector} from "@/store";
+import {peerActions, useAppDispatch, useAppSelector} from "@/store";
 import AudioCall from "@/components/screens/AudioCall";
 import {cn} from "@/lib/utils";
 import ScreenSend from "@/components/screens/ScreenSend";
-import StartScreen from "@/components/screens/StartScreen";
-import {Peer} from "@/types";
-import PeerItem from "@/components/homePage/PeerItem";
+import ActionBar from "@/components/connectPage/ActionBar";
+import {useEffect, useRef} from "react";
 
 function ConnectScreen() {
 
     const peer = useAppSelector(state => state.peer);
-    const {localVideoRef , remoteVideoRef, status , connectedPeers, signallingState , connectionState} = peer;
+    const {status, signallingState, connectionState} = peer;
 
-    console.log(connectedPeers);
+    const dispatch = useAppDispatch();
+
+    const localVideoRef = useRef<HTMLVideoElement | null>(null);
+    const remoteVideoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        dispatch(peerActions.setRemoteVideoRef(remoteVideoRef));
+        dispatch(peerActions.setLocalVideoRef(localVideoRef));
+    }, []);
+
 
     function renderScreen() {
         if (!status) {
-            return <StartScreen />
+            return <p className={"flex justify-center items-center text-xl flex-1"}>
+                برای شروع ارتباط یک از گزینه های زیر را انتخاب کنید
+            </p>
         } else if (signallingState === "have-local-offer") {
             return <p className={"flex justify-center items-center text-xl flex-1"}>
                 در انتظار پاسخ ...
             </p>
         } else if (status === "screen:send") {
-            return <ScreenSend peer={peer} />
+            return <ScreenSend peer={peer}/>
         } else if (status.startsWith("audio:") && connectionState === "connected") {
-            return <AudioCall peer={peer} />
+            return <AudioCall peer={peer}/>
         } else if (status.startsWith("video:") || status === "screen:receive") {
             return
         }
     }
 
     return (
-        <section className={"flex-1 flex overflow-hidden relative"}>
+        <section className={"flex-1 flex flex-col relative"}>
+
             {renderScreen()}
-            
-            <video ref={localVideoRef} controls autoPlay
+
+             <video ref={localVideoRef} controls autoPlay
                    className={cn("absolute top-5 right-5 w-64 h-36", {"hidden": !status?.startsWith("video:")})}/>
 
             <video ref={remoteVideoRef} controls autoPlay
                    className={cn("size-full", {"hidden": !status?.startsWith("video:") && status !== "screen:receive"})}/>
+            <ActionBar/>
         </section>
     )
 }
