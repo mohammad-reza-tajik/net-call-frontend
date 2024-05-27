07561cc91d3869b8e3cebc5f8aeee3e777bb4704
Morta <mr.tajik1999@gmail.com>
2024-05-27 17:01:35 +0330

    refactor: move all useInitalize code into Initialize component

--- components/shared/Initialize.tsx
+++ components/shared/Initialize.tsx
@@ -1,9 +1,37 @@
 "use client"
-import useInitialize from "@/hooks/useInitialize";
+import {useEffect} from "react";
+import localPeerIdSignal from "@/signals/peer/localPeerId";
+import createId from "@/utils/createId";
+import localStreamSignal from "@/signals/localStream";
+import devicesSignal from "@/signals/devices";
+import getDevices from "@/utils/getDevices";
+import {peerConnectionSignal} from "@/signals/peer/peerConnection";
+import createConnection from "@/utils/createConnection";
+import socketSignal from "@/signals/socket";
+import io from "socket.io-client";
+import getDeviceType from "@/utils/getDeviceType";
+import localPeerId from "@/signals/peer/localPeerId";
+import socketListeners from "@/utils/socketListeners";
+import peerConnectionListeners from "@/utils/peerConnectionListeners";
 
 function Initialize({children}: {children: React.ReactNode}) {
 
-    useInitialize();
+    useEffect(() => {
+        (async () => {
+            localPeerIdSignal.value = createId();
+            localStreamSignal.value = await navigator.mediaDevices.getUserMedia({audio: true});
+            devicesSignal.value = await getDevices();
+            peerConnectionSignal.value = createConnection();
+            socketSignal.value =io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
+                query: {
+                    deviceType: getDeviceType(),
+                    localPeerId
+                }
+            }).connect();
+            socketListeners();
+            peerConnectionListeners();
+        })();
+    }, []);
 
     return (
         <>
