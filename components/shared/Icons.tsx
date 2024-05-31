0e27af70f47069dbd89b861e9f70aa27d7752241
Morta <mr.tajik1999@gmail.com>
2024-05-31 11:33:06 +0330

    refactor(types): add I and T prefixes to all types

--- components/shared/Icons.tsx
+++ components/shared/Icons.tsx
@@ -318,6 +318,13 @@ export function Thumb(props: SVGProps<SVGSVGElement>) {
     )
 }
 
+
+export function Download(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...props}><path d="M224 144v64a8 8 0 0 1-8 8H40a8 8 0 0 1-8-8v-64a8 8 0 0 1 16 0v56h160v-56a8 8 0 0 1 16 0m-101.66 5.66a8 8 0 0 0 11.32 0l40-40a8 8 0 0 0-11.32-11.32L136 124.69V32a8 8 0 0 0-16 0v92.69L93.66 98.34a8 8 0 0 0-11.32 11.32Z"></path></svg>
+    )
+}
+
 export function Home(props: SVGProps<SVGSVGElement>) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M6 19h3v-5q0-.425.288-.712T10 13h4q.425 0 .713.288T15 14v5h3v-9l-6-4.5L6 10zm-2 0v-9q0-.475.213-.9t.587-.7l6-4.5q.525-.4 1.2-.4t1.2.4l6 4.5q.375.275.588.7T20 10v9q0 .825-.588 1.413T18 21h-4q-.425 0-.712-.288T13 20v-5h-2v5q0 .425-.288.713T10 21H6q-.825 0-1.412-.587T4 19m8-6.75"></path></svg>
--- types/index.ts
+++ types/index.ts
@@ -1,73 +1,44 @@
-import type {Socket} from "socket.io-client";
-import {MutableRefObject} from "react";
 
-export type Status = "screen:send" | "audio:send" | "audio:receive" | "video:send" | "video:receive" | "screen:receive";
+export type TStatus = "screen:send" | "audio:send" | "audio:receive" | "video:send" | "video:receive" | "screen:receive";
 
-export interface ConnectedPeer {
+export interface IConnectedPeer {
     socketId : string;
     localPeerId : string;
     deviceType: "desktop" | "mobile";
 }
 
-export interface Request {
+export interface IRequest {
     offer: RTCSessionDescriptionInit;
     localPeerId: string;
     remotePeerId: string;
     socketId: string;
-    status: Status;
+    status: TStatus;
     iceCandidates: RTCIceCandidate[];
 }
 
-export interface Response {
+export interface IResponse {
     answer: RTCSessionDescriptionInit;
     localPeerId: string;
     remotePeerId: string;
     socketId: string;
-    status: Status;
+    status: TStatus;
     iceCandidates: RTCIceCandidate[];
 }
 
-export interface Peer {
-    status?: Status;
-    connectedPeers : ConnectedPeer[];
-    connectionMode? : "group" | "pair";
-    localPeerId?: string;
-    remotePeerId?: string;
-    iceCandidates?: RTCIceCandidate[];
-    peerConnection?: RTCPeerConnection;
-    receivedRequests: Request[];
-    currentRequest? : Request;
-    currentResponse?: Response;
-    socket?: Socket;
-    offer?: RTCSessionDescriptionInit;
-    answer?: RTCSessionDescriptionInit;
-    localStream?: MediaStream;
-    remoteStream?: MediaStream;
-    signallingState? : RTCPeerConnection["signalingState"];
-    connectionState? : RTCPeerConnection["connectionState"];
-    localVideoRef?: MutableRefObject<HTMLVideoElement | null>;
-    remoteVideoRef?: MutableRefObject<HTMLVideoElement | null>;
-
-}
-
-export interface Devices {
-    audioInputs? : MediaDeviceInfo[];
-    audioOutputs? : MediaDeviceInfo[];
-    videoInputs? : MediaDeviceInfo[];
-    currentVideoInput? : MediaDeviceInfo;
-    currentAudioInput? : MediaDeviceInfo;
-    currentAudioOutput? : MediaDeviceInfo;
-}
-
-export interface TextMessage {
-    type: "text";
+export interface ITextMessage {
     text : string;
+    type: "text";
     localPeerId: string;
 }
 
-export interface FileMessage {
+export interface IFileData {
+    name : string;
+    mimeType : MIMEType;
+}
+
+export interface IFileMessage {
+    file : File;
     type: "file";
     localPeerId: string;
-    file : ArrayBuffer;
 }
 
