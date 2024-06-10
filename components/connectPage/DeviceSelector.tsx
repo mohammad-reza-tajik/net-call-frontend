17f4f1fb83f7d19d9303bdf77ebadd6e2be5bf1e
Morta <mr.tajik1999@gmail.com>
2024-06-10 21:01:47 +0330

    refactor(DeviceSelector): replace useState with useSignal

--- components/connectPage/DeviceSelector.tsx
+++ components/connectPage/DeviceSelector.tsx
@@ -1,44 +1,43 @@
 "use client"
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
-import {useState} from "react";
+import {useSignal} from "@preact/signals-react";
 
 interface Props {
     devices?: MediaDeviceInfo[];
-    text?:string;
+    text?: string;
 }
 
-function DeviceSelector({devices , text}: Props) {
+function DeviceSelector({devices, text}: Props) {
 
-    const [device, setDevice] = useState("default");
+    const device = useSignal("default");
 
     if (!devices) {
         return
     }
-    
+
     const sortChangeHandler = (value: string) => {
-        setDevice(value);
+        device.value = value;
     }
 
     return (
         <div className={"flex items-center gap-5"}>
             <p className={"text-sm"}>{text}</p>
-
-        <Select onValueChange={sortChangeHandler} defaultValue={device}>
-            <SelectTrigger className={"w-36 md:w-44"}>
-                <SelectValue>
-                    {device}
-                </SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-                {
-                    devices.map((item) => {
-                        return (
-                            <SelectItem value={item.deviceId} key={item.deviceId}>{item.label}</SelectItem>
-                        )
-                    })
-                }
-            </SelectContent>
-        </Select>
+            <Select onValueChange={sortChangeHandler} defaultValue={device.value}>
+                <SelectTrigger className={"w-36 md:w-44"}>
+                    <SelectValue>
+                        {device.value}
+                    </SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                    {
+                        devices.map((item) => {
+                            return (
+                                <SelectItem value={item.deviceId} key={item.deviceId}>{item.label}</SelectItem>
+                            )
+                        })
+                    }
+                </SelectContent>
+            </Select>
         </div>
     )
 }
