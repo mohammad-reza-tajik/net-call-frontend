0c61518604a091fb8de19cad6afe2e1d37468b02
Morta <mr.tajik1999@gmail.com>
2024-06-20 11:34:20 +0330

    feat: add close event listener for data channels

--- utils/createConnection.ts
+++ utils/createConnection.ts
@@ -2,12 +2,10 @@ import peerConnectionListeners from "@/utils/peerConnectionListeners";
 
 function createConnection() {
 
-    const peerConfig = {iceServers: [{urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"]}]};
+    const peerConfig : RTCConfiguration = {iceServers: [{urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"]}]};
     const peerConnection =  new RTCPeerConnection(peerConfig);
 
     peerConnectionListeners(peerConnection);
-
-    return peerConnection;
 
 }
 
--- utils/dataChannelListeners.ts
+++ utils/dataChannelListeners.ts
@@ -2,12 +2,16 @@
 function dataChannelListeners(dataChannel: RTCDataChannel) {
 
     dataChannel.addEventListener("open", () => {
-        console.log(`${dataChannel.label} channel is ready`)
+        console.log(`${dataChannel.label} channel is ready`);
     })
 
     dataChannel.addEventListener("error", (event) => {
         const errorEvent = event as RTCErrorEvent;
         console.log(errorEvent.error.message);
+    })
+
+    dataChannel.addEventListener("close", () => {
+        console.log(`${dataChannel.label} channel is closed`);
     })
 
 }
