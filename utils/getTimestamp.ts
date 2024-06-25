83a8ef8916c34b45e602960a37ad3d11ff0b60d7
Morta <mr.tajik1999@gmail.com>
2024-06-25 06:14:10 +0330

    refactor(getTimeStamp): show the time in 00 format
    previously it was like 6:13 or 5:1 but now its like 05:01

--- utils/getTimestamp.ts
+++ utils/getTimestamp.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 
 function getTimestamp(date : Date) {
-    return `${dayjs(date).hour()}:${dayjs(date).minute()}`;
+    return `${dayjs(date).hour().toString().padStart(2,"0")}:${dayjs(date).minute().toString().padStart(2,"0")}`;
 }
 
 export default getTimestamp
