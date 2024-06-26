686318bf73bca8732a1505ca51f8c08359493a21
Morta <mr.tajik1999@gmail.com>
2024-06-26 18:53:47 +0330

    feat: show an skeleton when peerId is not there yet

--- components/shared/PeerId.tsx
+++ components/shared/PeerId.tsx
@@ -5,6 +5,7 @@ import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/compon
 import {toast} from "react-toastify";
 import localPeerIdSignal from "@/signals/peer/localPeerId";
 import {useSignals} from "@preact/signals-react/runtime";
+import {Skeleton} from "@/components/ui/skeleton";
 
 function PeerId() {
 
@@ -19,20 +20,24 @@ function PeerId() {
         <TooltipProvider>
             <div className={"flex items-center justify-center gap-2 text-sm max-md:hidden"}>
                 <p>
-                آیدی شما :
+                    آیدی شما :
                 </p>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        {
-                            <Button variant={"outline"} onClick={copyIdHandler}>
-                                {localPeerIdSignal.value}
-                            </Button>
-                        }
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <p>برای کپی کردن آیدی کلیک کنید</p>
-                    </TooltipContent>
-                </Tooltip>
+                {
+                    !localPeerIdSignal.value ?
+                        <Skeleton className={"w-60 h-10"}/> :
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                {
+                                    <Button variant={"outline"} onClick={copyIdHandler}>
+                                        {localPeerIdSignal.value}
+                                    </Button>
+                                }
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>برای کپی کردن آیدی کلیک کنید</p>
+                            </TooltipContent>
+                        </Tooltip>
+                }
             </div>
         </TooltipProvider>
     )
