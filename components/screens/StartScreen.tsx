775a88f96b6ac568e9f3b02422993c7844f99766
Morta <mr.tajik1999@gmail.com>
2024-11-14 18:01:17 +0330

    chore: put PeerList in suspense because of useSearchParams hook

--- components/screens/StartScreen.tsx
+++ components/screens/StartScreen.tsx
@@ -1,6 +1,7 @@
-import { Pencil, Devices} from "@/components/shared/Icons";
+import {Pencil, Devices} from "@/components/shared/Icons";
 import PeerList from "@/components/homePage/PeerList";
 import PeerForm from "@/components/homePage/PeerForm";
+import {Suspense} from "react";
 
 function StartScreen() {
 
@@ -10,12 +11,14 @@ function StartScreen() {
                 <Devices className={"size-5 md:size-7"}/>
                 دستگاه های متصل
             </h2>
-            <PeerList />
+            <Suspense>
+                <PeerList/>
+            </Suspense>
             <h2 className={"flex items-center gap-3 py-1 md:py-3 text-sm md:text-lg"}>
                 <Pencil className={"size-5 md:size-7"}/>
                 وارد کردن آیدی دستگاه مقابل
             </h2>
-            <PeerForm />
+            <PeerForm/>
         </section>
     )
 }
