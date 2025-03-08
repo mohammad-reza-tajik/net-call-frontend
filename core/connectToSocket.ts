941094d25da74f53715091fb1ec5f5258c9f0fc0
Morta <mr.tajik1999@gmail.com>
2025-03-08 09:55:50 +0330

    feat: visibility as socket connection query

--- core/connectToSocket.ts
+++ core/connectToSocket.ts
@@ -4,12 +4,13 @@ import type {IConnectedPeer} from "@/types";
 
 interface IConnectToSocketParams extends Omit<IConnectedPeer, "socketId">{}
 
-function connectToSocket({localPeerId , deviceType} : IConnectToSocketParams) {
+function connectToSocket({localPeerId , deviceType , visibility} : IConnectToSocketParams) {
 
     const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
         query: {
             deviceType,
             localPeerId,
+            visibility
         }
     }).connect();
 
