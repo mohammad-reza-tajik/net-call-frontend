c47738e137ef926a2dedf5dcaee7ad6adc9dd4f1
Morta <mr.tajik1999@gmail.com>
2025-03-10 14:58:59 +0330

    feat: complete add to friends functionality

--- components/homePage/PeerList.tsx
+++ components/homePage/PeerList.tsx
@@ -4,12 +4,16 @@ import Pagination from "@/components/shared/Pagination";
 import {useSearchParams} from "next/navigation";
 import Table, {type Header} from "@/components/shared/Table";
 import connectedPeersSignal from "@/signals/peer/connectedPeers";
-import {useSignals} from "@preact/signals-react/runtime";
+import {useSignal, useSignals} from "@preact/signals-react/runtime";
 import type {IConnectedPeer} from "@/types";
 import {Button} from "@/components/ui/button";
-import Link from "next/link";
 import {UserPlus} from "@/components/shared/Icons";
-import ActionButton from "@/components/connectPage/ActionButton";
+import friendsSignal from "@/signals/peer/friends";
+import routerSignal from "@/signals/router";
+import {Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger,} from "@/components/ui/dialog";
+import {Input} from "@/components/ui/input";
+import toast from "react-hot-toast";
+
 
 const ITEMS_PER_PAGE = 5;
 
@@ -27,9 +31,11 @@ function PeerList() {
 
     const currentPage = useSearchParams().get("page") || 1;
 
+    const nameInputSignal = useSignal("");
+
     const currentItems = connectedPeersSignal.value.slice((+currentPage - 1) * ITEMS_PER_PAGE, +currentPage * ITEMS_PER_PAGE);
 
-    function sortByHeaderHandler(header: string) {
+    const sortByHeaderHandler = (header: string) => {
         connectedPeersSignal.value = connectedPeersSignal.value.toSorted((a, b) => {
             // @ts-expect-error since headers are of type any
             if (a[header] < b[header]) return -1;
@@ -37,9 +43,9 @@ function PeerList() {
             if (a[header] > b[header]) return 1;
             return 0;
         });
-    }
+    };
 
-    function headersToShowHandler() {
+    const headersToShowHandler = () => {
         const keys = Object.keys(currentItems[0]) as Array<keyof IConnectedPeer>;
         const headers = keys.map((item) => {
             if (item === "socketId" || item === "visibility") return;
@@ -48,28 +54,62 @@ function PeerList() {
 
         });
         return [...headers, {label: "--actionButtons"}];
-    }
+    };
 
-    function renderCellHandler(header: Header, data: string, dataItem: Record<keyof IConnectedPeer, unknown>) {
+    const addToFriendsHandler = (dataItem: Record<keyof IConnectedPeer, any>) => {
+
+        if(nameInputSignal.value.trim() === "") {
+            return toast.error("نام نمیتواند خالی باشد");
+        }
+
+        if (friendsSignal.value.find((item) => item.name === nameInputSignal.value)) {
+            return toast.error("دوستی با این نام از قبل وجود دارد");
+        }
+
+        friendsSignal.value = [...friendsSignal.value, {
+            name: nameInputSignal.value,
+            isOnline: true,
+            localPeerId: dataItem.localPeerId
+        }];
+        
+        connectedPeersSignal.value = connectedPeersSignal.value.filter((item) => item.localPeerId !== dataItem.localPeerId);
+        nameInputSignal.value = "";
+        toast("به دوستان افزوده شد");
+    };
+
+    const renderCellHandler = (header: Header, data: string, dataItem: Record<keyof IConnectedPeer, any>) => {
         if (header.label === "--actionButtons") {
             return (
                 <div className={"flex items-center gap-2 justify-end"}>
-                    <Button asChild>
-                        <Link href={`/connect?remotePeerId=${dataItem.localPeerId}`}>
-                            اتصال
-                        </Link>
+                    <Button onClick={() => routerSignal.value?.push(`/connect?remotePeerId=${dataItem.localPeerId}`)}>
+                        اتصال
                     </Button>
 
-                    <ActionButton icon={<UserPlus className={"size-5"}/>} tooltipContent={"افزودن به دوستان"}
-                                  handler={() => {
-                                      console.log("add to friend");
-                                  }}/>
-
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button size={"icon"} variant={"outline"}>
+                                <UserPlus className={"size-5"} aria-label={"افزودن به دوستان"}/>
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle className={"text-center text-xs md:text-sm"}>
+                                    لطفا نامی برای این دوست انتخاب کنید
+                                </DialogTitle>
+                            </DialogHeader>
+                            <Input value={nameInputSignal.value}
+                                   onChange={ev => nameInputSignal.value = ev.target.value}/>
+                            <Button onClick={() => addToFriendsHandler(dataItem)}
+                                    className={"max-w-max mx-auto"}>
+                                ذخیره
+                            </Button>
+                        </DialogContent>
+                    </Dialog>
                 </div>
             );
         }
         return data;
-    }
+    };
 
     return (
         <>
