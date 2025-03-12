1a6f33b6d3fb91adb209cbdde2ead0544ef32821
Morta <mr.tajik1999@gmail.com>
2025-03-12 15:52:31 +0330

    feat: make stored localPeerId and visibility type-safe
    with this if somebody changes localPeerId or visibility in the local storage it will be fixed

--- components/shared/Initializer.tsx
+++ components/shared/Initializer.tsx
@@ -1,39 +1,39 @@
 "use client";
-import {Suspense, useEffect} from "react";
+import { Suspense, useEffect } from "react";
 import localPeerIdSignal from "@/signals/peer/localPeerId";
 import localStreamSignal from "@/signals/localStream";
 import iODevicesSignal from "@/signals/iODevices";
 import getIODevices from "@/core/getIODevices";
 import createConnection from "@/core/createConnection";
 import socketSignal from "@/signals/socket";
-import {useSignal, useSignalEffect} from "@preact/signals-react/runtime";
-import {peerConnectionSignal} from "@/signals/peer/peerConnection";
-import {useRouter} from "next/navigation";
+import { useSignal, useSignalEffect } from "@preact/signals-react/runtime";
+import { peerConnectionSignal } from "@/signals/peer/peerConnection";
+import { useRouter } from "next/navigation";
 import routerSignal from "@/signals/router";
-import {batch} from "@preact/signals-react";
+import { batch } from "@preact/signals-react";
 import connectToSocket from "@/core/connectToSocket";
 import HangupOnRouteChange from "@/components/shared/HangupOnRouteChange";
-import {type Toast, toast} from "react-hot-toast";
-import {Button} from "@/components/ui/button";
+import { type Toast, toast } from "react-hot-toast";
+import { Button } from "@/components/ui/button";
 import getDeviceType from "@/core/getDeviceType";
 import visibilitySignal from "@/signals/peer/visibility";
-import type {IConnectedPeer} from "@/types";
+import type { IConnectedPeer, TVisibility } from "@/types";
 import friendsSignal from "@/signals/peer/friends";
+import isValidUUID from "@/lib/utils/isValidUUID";
 
 interface IInitializerProps {
     children: React.ReactNode;
 }
 
-function Initializer({children}: IInitializerProps) {
-
+function Initializer({ children }: IInitializerProps) {
     routerSignal.value = useRouter();
 
     /*
-    Boolean signal to track completion of initial setup, including localStorage loading.
-    Ensures useSignalEffect hooks only save to localStorage after data is fully loaded,
-     preventing overwrite with default or empty values on mount.
+        Boolean signal to track completion of initial setup, including localStorage loading.
+        Ensures useSignalEffect hooks only save to localStorage after data is fully loaded,
+        preventing overwrite with default or empty values on mount.
      */
-    const isLoaded = useSignal(false);
+    const isLoadedSignal = useSignal(false);
 
     useEffect(() => {
         (async () => {
@@ -51,24 +51,34 @@ function Initializer({children}: IInitializerProps) {
 
                 // this is because we need to request notification permission on a user gesture
                 if (Notification.permission !== "granted") {
-                    toast((t: Toast) => (
-                        <div className={"flex items-center gap-2"}>
-                            <p>دسترسی به اعلان مورد نیاز است</p>
-                            <Button size={"sm"} className={"text-xs"} onClick={async () => {
-                                const permission = await Notification.requestPermission();
-                                if (permission !== "granted") {
-                                    throw new Error("دسترسی به اعلان مورد نیاز است");
-                                }
-                                toast.dismiss(t.id);
-                            }}>
-                                دادن مجوز
-                            </Button>
-                        </div>
-                    ), {id: "notification-permission", duration: Infinity});
-
+                    toast(
+                        (t: Toast) => (
+                            <div className={"flex items-center gap-2"}>
+                                <p>دسترسی به اعلان مورد نیاز است</p>
+                                <Button
+                                    size={"sm"}
+                                    className={"text-xs"}
+                                    onClick={async () => {
+                                        const permission = await Notification.requestPermission();
+                                        if (permission !== "granted") {
+                                            throw new Error("دسترسی به اعلان مورد نیاز است");
+                                        }
+                                        toast.dismiss(t.id);
+                                    }}
+                                >
+                                    دادن مجوز
+                                </Button>
+                            </div>
+                        ),
+                        { id: "notification-permission", duration: Infinity },
+                    );
                 }
 
-                const visibility = (localStorage.getItem("visibility") || "visible") as IConnectedPeer["visibility"];
+                let visibility = localStorage.getItem("visibility") as TVisibility;
+
+                if (visibility !== "visible" && visibility !== "hidden") {
+                    visibility = "visible";
+                }
 
                 const storedFriends = localStorage.getItem("friends");
 
@@ -79,13 +89,13 @@ function Initializer({children}: IInitializerProps) {
                 }
 
                 let localPeerId = localStorage.getItem("localPeerId");
-                if (!localPeerId) {
+                if (!localPeerId || !isValidUUID(localPeerId)) {
                     localPeerId = localStream.id;
                     localStorage.setItem("localPeerId", localPeerId);
                 }
                 const deviceType = getDeviceType();
                 const devices = await getIODevices();
-                const socket = connectToSocket({localPeerId, deviceType, visibility});
+                const socket = connectToSocket({ localPeerId, deviceType, visibility });
 
                 batch(() => {
                     localPeerIdSignal.value = localPeerId;
@@ -94,8 +104,7 @@ function Initializer({children}: IInitializerProps) {
                     socketSignal.value = socket;
                     visibilitySignal.value = visibility;
                     friendsSignal.value = friends;
-                    isLoaded.value = true;
-
+                    isLoadedSignal.value = true;
                 });
             } catch (err) {
                 if (err instanceof Error) {
@@ -111,7 +120,7 @@ function Initializer({children}: IInitializerProps) {
             const newSocket = connectToSocket({
                 localPeerId: localPeerIdSignal.value,
                 visibility: visibilitySignal.peek(),
-                deviceType: getDeviceType()
+                deviceType: getDeviceType(),
             });
             socketSignal.value = newSocket;
         }
@@ -124,7 +133,7 @@ function Initializer({children}: IInitializerProps) {
     });
 
     useSignalEffect(() => {
-        if (isLoaded.value) {
+        if (isLoadedSignal.value) {
             localStorage.setItem("friends", JSON.stringify(friendsSignal.value));
         }
     });
@@ -132,7 +141,7 @@ function Initializer({children}: IInitializerProps) {
     return (
         <>
             <Suspense>
-                <HangupOnRouteChange/>
+                <HangupOnRouteChange />
             </Suspense>
             {children}
         </>
