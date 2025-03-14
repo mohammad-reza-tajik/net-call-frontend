6d3f458fa9bd31c2272bc82c79da4cf961a14c4d
Morta <mr.tajik1999@gmail.com>
2025-03-14 05:35:02 +0330

    refactor: use status signal to hangup on route change

--- components/shared/HangupOnRouteChange.tsx
+++ components/shared/HangupOnRouteChange.tsx
@@ -3,15 +3,16 @@ import {useEffect} from "react";
 import {peerConnectionSignal} from "@/signals/peer/peerConnection";
 import socketSignal from "@/signals/socket";
 import localPeerIdSignal from "@/signals/peer/localPeerId";
+import statusSignal from "@/signals/peer/status";
 
 function HangupOnRouteChange() {
     const pathName = usePathname();
     const searchParams = useSearchParams();
 
     useEffect(() => {
-        if (peerConnectionSignal.value?.connectionState === "connected" || peerConnectionSignal.value?.connectionState === "connecting") {
+        if (statusSignal.value) {
             socketSignal.value?.emit("hangupToServer", {localPeerId: localPeerIdSignal.value});
-            peerConnectionSignal.value.close();
+            peerConnectionSignal.value?.close();
         }
     }, [pathName, searchParams]);
 
