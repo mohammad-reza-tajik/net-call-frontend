01968337fe212b97857036063a38021e8ab73667
Morta <mr.tajik1999@gmail.com>
2025-03-23 13:14:36 +0330

    refactor: use zod in peer form comp

--- components/homePage/PeerForm.tsx
+++ components/homePage/PeerForm.tsx
@@ -1,50 +1,57 @@
 "use client";
-import {toast} from "react-hot-toast";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {useRef} from "react";
-import {useSignals} from "@preact/signals-react/runtime";
+import { toast } from "react-hot-toast";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+import { useSignals } from "@preact/signals-react/runtime";
 import remotePeerIdSignal from "@/signals/peer/remotePeerId";
 import routerSignal from "@/signals/router";
 import localPeerIdSignal from "@/signals/peer/localPeerId";
-import isValidUUID from "@/lib/utils/isValidUUID";
 import buildURL from "@/lib/utils/buildURL";
+import { z } from "zod";
 
 function PeerForm() {
+  useSignals();
+  const remotePeerIdRef = useRef<HTMLInputElement>(null);
 
-    useSignals();
-    const remotePeerIdRef = useRef<HTMLInputElement>(null);
+  const submitRemotePeerIdHandler = () => {
+    try {
+      if (!remotePeerIdRef.current?.value) {
+        return;
+      }
 
-    const submitRemotePeerIdHandler = () => {
-        if (!remotePeerIdRef.current?.value) {
-            return;
-        }
+      const validatedLocalPeerId = z.string().uuid().parse(remotePeerIdRef.current.value);
 
-        if (!isValidUUID(remotePeerIdRef.current.value) || remotePeerIdRef.current.value === localPeerIdSignal.value) {
-            toast.error("آیدی وارد شده صحیح نیست");
-            return;
-        }
+      if (validatedLocalPeerId === localPeerIdSignal.value) {
+        throw new Error("آیدی وارد شده صحیح نیست");
+      }
 
-        const peerURL = buildURL({
-            query: {
-                remotePeerId:remotePeerIdRef.current.value
-            },
-            url : "/connect"
-        });
+      const peerURL = buildURL({
+        query: {
+          remotePeerId: validatedLocalPeerId,
+        },
+        url: "/connect",
+      });
 
-        remotePeerIdSignal.value = remotePeerIdRef.current.value;
-        remotePeerIdRef.current.value = "";
-        routerSignal.value!.push(peerURL);
-    };
+      remotePeerIdSignal.value = validatedLocalPeerId;
+      remotePeerIdRef.current.value = "";
+      routerSignal.value!.push(peerURL);
+    } catch (err) {
+      if (err instanceof Error) {
+        toast.error(err.message);
+        console.error(err);
+      }
+    }
+  };
 
-    return (
-        <div className={"flex flex-col justify-center items-center py-5 md:py-10 gap-5 border rounded"}>
-            <Input className={"w-3/4 md:w-80 p-7"} ref={remotePeerIdRef} placeholder={"آیدی دستگاه ..."}/>
-            <Button onClick={submitRemotePeerIdHandler} size={"lg"} variant={"outline"}>
-                    تایید
-            </Button>
-        </div>
-    );
+  return (
+    <div className={"flex flex-col justify-center items-center py-5 md:py-10 gap-5 border rounded"}>
+      <Input className={"w-3/4 md:w-80 p-7"} ref={remotePeerIdRef} placeholder={"آیدی دستگاه ..."} />
+      <Button onClick={submitRemotePeerIdHandler} size={"lg"} variant={"outline"}>
+        تایید
+      </Button>
+    </div>
+  );
 }
 
 export default PeerForm;
