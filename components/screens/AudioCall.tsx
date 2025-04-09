b81fa55eae760330832425afebc30e2c47105b84
Morta <mr.tajik1999@gmail.com>
2025-04-09 21:39:24 +0330

    feat: use ripple component in Audio call screen

--- components/screens/AudioCall.tsx
+++ components/screens/AudioCall.tsx
@@ -1,24 +1,25 @@
-import { ArrowsLeftRight, Phone } from "@/components/shared/Icons";
-import cn from "@/lib/utils/cn";
-import { Fragment } from "react";
+"use client";
+import { Phone } from "@/components/shared/Icons";
+import remoteStreamSignal from "@/signals/remoteStream";
+import useVoiceDetection from "@/hooks/useVoiceDetection";
+import { Ripple } from "@/components/magicui/ripple";
+import { useSignals } from "@preact/signals-react/runtime";
+
 
 function AudioCall() {
-    return (
-        <div className={"flex flex-col md:flex-row size-full justify-center items-center gap-2 md:gap-5"}>
-            {Array.from({ length: 2 }).map((_, index) => {
-                return (
-                    <Fragment key={index}>
-                        <div className={"flex justify-center items-center size-52 lg:size-96 border rounded"}>
-                            <Phone className={"size-14 md:size-20"} />
-                        </div>
-                        <ArrowsLeftRight
-                            className={cn("max-md:rotate-90 size-10 self-center", { hidden: index !== 0 })}
-                        />
-                    </Fragment>
-                );
-            })}
-        </div>
-    );
+  
+  useSignals();
+
+  const remoteSpeaking = useVoiceDetection(remoteStreamSignal.value!);
+
+  return (
+    <div className={"flex flex-col size-full"}>
+      <div className={"flex justify-center items-center relative flex-1"}>
+        {remoteSpeaking && <Ripple numCircles={3} />}
+        <Phone className={"size-14 md:size-20"} />
+      </div>
+    </div>
+  );
 }
 
 export default AudioCall;
